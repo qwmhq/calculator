bcf77584c145333efbb1f630a4284ae422df2e8d
Quwam Ogunlaja <quwam32@gmail.com>
2021-12-23 14:27:24 +0100

    Added 'operate' function

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 }
+
+const operate = function(a, b, op) {
+    if (op === '+') {
+        return add(a, b);
+    } else if (op === '-') {
+        return subtract(a, b);
+    } else if (op === '*') {
+        return multiply(a, b);
+    } else if (op === '/') {
+        return divide(a, b);
+    } else return 'Invalid operator';
+}
